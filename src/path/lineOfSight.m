ecda64427ccf16ab4dda3058a8307018fe22ac61
Andreas Nordby Vibeto <andreanv@ntnu.no>
2016-12-17 18:11:56 +0100

    SRC: Altered path now works

---
+++ src/path/lineOfSight.m
@@ -0,0 +1,16 @@
+function chi_c = lineOfSight(pos, path)
+    R = 150;
+    cont = 1;
+    
+    for i = (length(path(:,1)):-1:1)
+        radius = (path(i,1)-pos(1))^2 + (path(i,2)-pos(2))^2;
+        if (radius > (R - 10)^2) && (radius < (R + 10)^2)
+            cont = 0;
+            break;
+        end
+        if(~cont)
+            break;
+        end
+    end
+    
+    chi_c = atan2(path(i,2)-pos(2), path(i,1)-pos(1));
