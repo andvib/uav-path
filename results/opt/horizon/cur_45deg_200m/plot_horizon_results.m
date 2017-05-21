2f8ecddbc2e0a1a9ebe2d0df7f368c2e583b158b
Andreas Nordby Vibeto <andreanv@ntnu.no>
2017-05-21 09:56:51 +0200

    RES: Correct results for horizon_45deg_200m

--- results/opt/horizon/cur_45deg_200m/plot_horizon_results.m
+++ results/opt/horizon/cur_45deg_200m/plot_horizon_results.m
@@ -8,7 +8,7 @@ figure(1);
 grid on;
 hold on;
 title('POSITION');
-for i = 110%80:10:120
+for i = 20:10:120
     a = sprintf('results_horizon/horizon_%d.m', i);
     run (a)
     
@@ -24,7 +24,7 @@ figure(2);
 grid on;
 hold on;
 title('CAMERA CENTRE POINT');
-for i = 110%80:10:120
+for i = 20:10:120
     a = sprintf('results_horizon/horizon_%d.m', i);
     run (a)
     
@@ -50,7 +50,7 @@ figure(3);
 grid on;
 hold on;
 title('HEIGHT');
-for i = 110%80:10:120
+for i = 20:10:120
     a = sprintf('results_horizon/horizon_%d.m', i);
     run (a)
     
@@ -67,7 +67,7 @@ grid on;
 hold on;
 title('ATTITUDE');
 k = 1;
-for i = 110%80:10:120
+for i = 20:10:120
     a = sprintf('results_horizon/horizon_%d.m', i);
     run (a)
     
@@ -91,7 +91,7 @@ grid on;
 hold on;
 title('CONTROl');
 k = 1;
-for i = 110%80:10:120
+for i = 20:10:120
     a = sprintf('results_horizon/horizon_%d.m', i);
     run (a)
     
@@ -118,7 +118,7 @@ grid on;
 hold on;
 title('DURATION');
 k = 1;
-for i = 110%80:10:120
+for i = 20:10:120
     a = sprintf('results_horizon/horizon_%d.m', i);
     run (a)
     
