94a23ee4569f9c7f13a4bfd6448efdce0bf94a8c
Andreas Nordby Vibeto <andreanv@ntnu.no>
2017-05-26 19:38:24 +0200

    DOC: Added short section on nonlinearity

--- src/pathgen/genpath.m
+++ src/pathgen/genpath.m
@@ -1,11 +1,11 @@
 close all;
 clear all;
 
-wpp_lin = [0 0; 400 0 ; 400 300 ; 2000 300];
+wpp_lin = [0 0; 400 0 ; 400 200 ; 2000 200];
 path_lin = generateLinearPath(wpp_lin,0.1);
 
-wpp_cur = [[0,0,0,0]; [250,0,0,0]; [550,300,0,0];[2000, 300, 0,0]];
-path_cur = generateDubinsPath(wpp_cur, 0.1, 150);
+wpp_cur = [[0,0,0,0]; [300,0,0,0]; [500,200,0,0];[2000, 200, 0,0]];
+path_cur = generateDubinsPath(wpp_cur, 0.1, 100);
 
 %path_lin = csvread('path_lin_90deg_2turns.txt');
 %path_cur = csvread('path_cur_90deg_150m_2turns.txt');
@@ -18,5 +18,5 @@ plot(path_cur(:,2), path_cur(:,1));
 xlim([-200 800]);
 ylim([0 1000]);
 
-%csvwrite('path_cur_90deg_150m_2turns.txt',path_cur);
-%csvwrite('path_lin_90deg_2turns.txt',path_lin);
+csvwrite('path_cur_90deg_100m_2turns.txt',path_cur);
+csvwrite('path_lin_90deg_2turns.txt',path_lin);
