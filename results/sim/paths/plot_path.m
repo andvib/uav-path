ff3b38f576762d42c628c81a9b0799803dcb70ec
Andreas Nordby Vibeto <andreanv@ntnu.no>
2017-06-01 15:33:16 +0200

    DOC: Added section on paths

--- results/sim/paths/plot_path.m
+++ results/sim/paths/plot_path.m
@@ -2,104 +2,98 @@ close all;
 clear all;
 
 
-%% PLOT POSITION %%
-run 'path_lin_easy.m'
+%% Read Data %%
+run 'results_paths/path_lin_easy.m';
+time_lin = STATES(:,1);
+path_lin = [PATH(:,1) PATH(:,2)];
+pos_lin = [STATES(:,2) STATES(:,3)];
+alt_lin = STATES(:,4);
+phi_lin = STATES(:,8);
+theta_lin = STATES(:,9);
+psi_lin = STATES(:,10);
+
+run 'results_paths/path_cur_easy.m';
+time_cur = STATES(1:577,1);
+path_cur = [PATH(:,1) PATH(:,2)];
+pos_cur = [STATES(1:577,2) STATES(1:577,3)];
+alt_cur = STATES(1:577,4);
+phi_cur = STATES(1:577,8);
+theta_cur = STATES(1:577,9);
+psi_cur = STATES(1:577,10);
+
+
+
+%% PLOT PATHS %%
+
 figure(1);
 grid on;
 hold on;
-plot(STATES(:,3),STATES(:,2));
-plot(PATH(:,2), PATH(:,1),'k');
-for j = (1:length(STATES(:,1)))
-    [x_temp, y_temp] = camera_pos([STATES(j,8), STATES(j,9), STATES(j,10)],...
-                        [STATES(j,2), STATES(j,3), STATES(j,4)], 0.0);%0.331612);
+plot(path_lin(:,2), path_lin(:,1));
+plot(path_cur(:,2), path_cur(:,1),'k');
+ylabel('North [m]');
+xlabel('East [m]');
+ylim([0 1200]);
+xlim([-300 900]);
+saveas(gcf, 'fig/path', 'epsc');
+
+
+%% PLOT LINEAR %%
+
+figure(2);
+grid on;
+hold on;
+plot(path_lin(:,2), path_lin(:,1),'k');
+plot(pos_lin(:,2), pos_lin(:,1));
+ylabel('North [m]');
+xlabel('East [m]');
+ylim([0 1200]);
+xlim([-300 900]);
+saveas(gcf, 'fig/uav_pos_lin', 'epsc');
+
+figure(3);
+grid on;
+hold on;
+plot(path_lin(:,2), path_lin(:,1),'k');
+for j = (1:length(pos_lin(:,1)))
+    [x_temp, y_temp] = camera_pos([phi_lin(j), theta_lin(j), psi_lin(j)],...
+                        [pos_lin(j,1), pos_lin(j,2), alt_lin(j)], 0.0);%0.331612);
     c_n_1(:,j) = x_temp;
     c_n_2(:,j) = y_temp;
 end
 plot(c_n_1(2,:), c_n_1(1,:),'color',[1 .4 0]);
 ylabel('North [m]');
 xlabel('East [m]');
-xlim([-300 900]);
 ylim([0 1200]);
-set(gca, 'fontsize', 14);
-%respath = sprintf('fig_cur/uav_position_%ddeg_150m', i);
-%saveas(gcf, respath, 'epsc');
-%close all;
+xlim([-300 900]);
+saveas(gcf, 'fig/camera_pos_lin', 'epsc');
 
-%% PLOT CAMERA CENTRE POINT%%
 
-% for i = angles
-%     a = sprintf('results/path_cur_%ddeg_2turns_150m.m', i);
-%     run (a);
-%     
-%     figure(2);
-%     grid on;
-%     hold on;
-%     for j = (1:length(STATES(:,1)))
-%         [x_temp, y_temp] = camera_pos([STATES(j,8), STATES(j,9), STATES(j,10)],...
-%                             [STATES(j,2), STATES(j,3), STATES(j,4)], 0.0);%0.331612);
-%         c_n_1(:,j) = x_temp;
-%         c_n_2(:,j) = y_temp;
-%     end
-%     
-%     plot(c_n_1(2,:), c_n_1(1,:),'color',[1 .4 0]);
-%     plot(PATH(:,2), PATH(:,1),'k');
-%     
-%     ylabel('North [m]');
-%     xlabel('East [m]');
-%     
-%     xlim([-100 400]);
-%     ylim([300 800]);
-%     set(gca, 'fontsize', 14);
-%     respath = sprintf('fig_cur/camera_position_%ddeg_150m', i);
-%     saveas(gcf, respath, 'epsc');
-%     close all;
-% end
-% 
-% %% PLOT HEIGHT %%
-% 
-% figure(3);
-% grid on;
-% hold on; 
-% plot(STATES(:,1), STATES(:,4));
-% xlim([0 40]);
-% ylim([100 160]);
-% ylabel('Height [m]');
-% xlabel('Time [s]');
-% %matlab2tikz('fig/height.tex');
-% %saveas(gcf, 'fig_45deg/height', 'epsc');
-% 
-% %% PLOT ATTITUDE %%
-% 
-% figure(4);
-% grid on;
-% hold on;
-% for i = angles
-%     a = sprintf('results/path_cur_%ddeg_2turns_150m.m', i);
-%     run (a)
-%     
-%     %subplot(3,1,1);
-%     grid on;
-%     hold on;
-%     plot(STATES(:,1), STATES(:,10));
-%     ylabel('\psi [rad]');
-%     xlabel('Time [s]');
-% end
-% legend('45 degree', '70 degree');
-% set(gca, 'fontsize', 14);
-% saveas(gcf, 'fig_cur/heading', 'epsc');
-% 
-% %% PLOT CONTROLS %%
-% 
-% figure(5);
-% grid on;
-% hold on;
-% 
-% subplot(4,1,1);
-% plot(STATES(:,1), STATES(:,14));
-% subplot(4,1,2);
-% plot(STATES(:,1), STATES(:,15));
-% subplot(4,1,3);
-% plot(STATES(:,1), STATES(:,16));
-% subplot(4,1,4);
-% plot(STATES(:,1), STATES(:,17));
-% %saveas(gcf, 'fig_45deg/control','epsc');
+%% PLOT CURVED %%
+
+figure(4);
+grid on;
+hold on;
+plot(path_cur(:,2), path_cur(:,1),'k');
+plot(pos_cur(:,2), pos_cur(:,1));
+ylabel('North [m]');
+xlabel('East [m]');
+ylim([0 1200]);
+xlim([-300 900]);
+saveas(gcf, 'fig/uav_pos_cur', 'epsc');
+
+figure(5);
+grid on;
+hold on;
+plot(path_cur(:,2), path_cur(:,1),'k');
+for j = (1:length(pos_cur(:,1)))
+    [x_temp, y_temp] = camera_pos([phi_cur(j), theta_cur(j), psi_cur(j)],...
+                        [pos_cur(j,1), pos_cur(j,2), alt_cur(j)], 0.0);%0.331612);
+    c_n_1(:,j) = x_temp;
+    c_n_2(:,j) = y_temp;
+end
+plot(c_n_1(2,:), c_n_1(1,:),'color',[1 .4 0]);
+ylabel('North [m]');
+xlabel('East [m]');
+ylim([0 1200]);
+xlim([-300 900]);
+saveas(gcf, 'fig/camera_pos_cur', 'epsc');
