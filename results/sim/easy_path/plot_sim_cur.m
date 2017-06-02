91d99a565f1e30f740afbf64f193f177b9d0d10b
Andreas Nordby Vibeto <andreanv@ntnu.no>
2017-06-02 17:22:08 +0200

    DOC: Wrote results on simulation

--- results/sim/easy_path/plot_sim_cur.m
+++ results/sim/easy_path/plot_sim_cur.m
@@ -57,7 +57,7 @@ plot(PATH(:,2), PATH(:,1),'k');
 plot(path_y, path_x);
 for i = (1:length(path_phi))
     [x_temp, y_temp] = camera_pos([path_phi(i), path_theta(i), path_psi(i)],...
-                                  [path_x(i), path_y(i), path_hgt(i)], deg2rad(19));
+                                  [path_x(i), path_y(i), path_hgt(i)], deg2rad(0));
     c_n_1(:,i) = x_temp;
     c_n_2(:,i) = y_temp;
 end
@@ -68,6 +68,8 @@ xlim([-300 900]);
 ylim([0 1200]);
 xlabel('East [m]');
 ylabel('North [m]');
+set(gca, 'fontsize', 14);
+saveas(gcf, 'fig_cur/path_run', 'epsc');
 
 %% PLOT POS RUN %%
 figure(2);
@@ -77,39 +79,95 @@ plot(PATH(:,2), PATH(:,1),'k');
 plot(pos_y, pos_x);
 for i = (1:length(pos_phi))
     [x_temp, y_temp] = camera_pos([pos_phi(i), pos_theta(i), pos_psi(i)],...
-                                  [pos_x(i), pos_y(i), pos_hgt(i)], deg2rad(19));
+                                  [pos_x(i), pos_y(i), pos_hgt(i)], deg2rad(0));
     c_n_1(:,i) = x_temp;
     c_n_2(:,i) = y_temp;
 end
     
 plot(c_n_1(2,:),c_n_1(1,:),'color',[1 .4 0]);
 plot(c_n_2(2,:),c_n_2(1,:),'color',[1 .4 0]);
+
+plot(pos_y(1350), pos_x(1350),'*');
+
 xlim([-300 900]);
 ylim([0 1200]);
 xlabel('East [m]');
 ylabel('North [m]');
+set(gca, 'fontsize', 14);
+saveas(gcf, 'fig_cur/pos_run', 'epsc');
 
 
+%% PLOT TRACKING PERF %%
 
-%% CALCULATE CUR ERROR %%
+figure(3);
+hold on;
+grid on;
+plot(STATES(:,3), STATES(:,2));
+plot(pos_y, pos_x);
+legend('Optimized path','Flown path', 'Location', 'SE');
+xlim([-300 900]);
+ylim([0 1200]);
+set(gca, 'fontsize', 14);
+saveas(gcf, 'fig_cur/tracking', 'epsc');
 
-for i = 1:length(pos_x)
-    [x_temp, y_temp] = camera_pos([pos_phi(i), pos_theta(i), pos_psi(i)],...
-                        [pos_x(i), pos_y(i), pos_hgt(i)], 0.0);
-    
-    min_error = 100000;
-    
-    for j = 1:length(PATH(:,1))
-        dist = sqrt((PATH(j,1) - x_temp(1))^2 + (PATH(j,2) - x_temp(2))^2);
-        if dist < min_error
-            min_error = dist;
-        end
+
+%% CALCULATE path ERROR %%
+path_started = 0;
+for i = 1:length(path_x(1:1350))
+    if((path_x(i) >= 0) && (~path_started))
+        path_started = 1
     end
-    cur_error(i) = min_error;
+    
+    if(path_started)
+        [x_temp, y_temp] = camera_pos([path_phi(i), path_theta(i), path_psi(i)],...
+                        [path_x(i), path_y(i), path_hgt(i)], 0.0);
+    
+        min_error = 100000;
+    
+        for j = 1:length(PATH(:,1))
+            dist = sqrt((PATH(j,1) - x_temp(1))^2 + (PATH(j,2) - x_temp(2))^2);
+            if dist < min_error
+                min_error = dist;
+            end
+        end
+        if(min_error > 46)
+            i
+        end
+        path_error(i) = min_error;
+    end
 end
 
-%figure(12)
-%plot(time_cur, cur_error)
+mean_path_error = mean(path_error)
+max_path_error = max(path_error)
+min_path_error = min(path_error)
+std_path_error = std(path_error)
+
+%% CALCULATE CUR ERROR %%
+path_started = 0;
+for i = 1:length(pos_x(1:1350))
+    if((pos_x(i) >= 0) && (~path_started))
+        path_started = 1
+        i
+    end
+    
+    if(path_started)
+        [x_temp, y_temp] = camera_pos([pos_phi(i), pos_theta(i), pos_psi(i)],...
+                        [pos_x(i), pos_y(i), pos_hgt(i)], 0.0);
+    
+        min_error = 100000;
+    
+        for j = 1:length(PATH(:,1))
+            dist = sqrt((PATH(j,1) - x_temp(1))^2 + (PATH(j,2) - x_temp(2))^2);
+            if dist < min_error
+                min_error = dist;
+            end
+        end
+        if(min_error > 46)
+            i
+        end
+        cur_error(i) = min_error;
+    end
+end
 
 mean_cur_error = mean(cur_error)
 max_cur_error = max(cur_error)
--- results/sim/easy_path/plot_sim_lin.m
+++ results/sim/easy_path/plot_sim_lin.m
@@ -57,7 +57,7 @@ plot(PATH(:,2), PATH(:,1),'k');
 plot(path_y, path_x);
 for i = (1:length(path_phi))
     [x_temp, y_temp] = camera_pos([path_phi(i), path_theta(i), path_psi(i)],...
-                                  [path_x(i), path_y(i), path_hgt(i)], deg2rad(19));
+                                  [path_x(i), path_y(i), path_hgt(i)], deg2rad(0));
     c_n_1(:,i) = x_temp;
     c_n_2(:,i) = y_temp;
 end
@@ -68,6 +68,8 @@ xlim([-300 900]);
 ylim([0 1200]);
 xlabel('East [m]');
 ylabel('North [m]');
+%set(gca, 'fontsize', 14);
+saveas(gcf, 'fig_lin/path_run', 'epsc');
 
 %% PLOT POS RUN %%
 figure(2);
@@ -77,14 +79,94 @@ plot(PATH(:,2), PATH(:,1),'k');
 plot(pos_y, pos_x);
 for i = (1:length(pos_phi))
     [x_temp, y_temp] = camera_pos([pos_phi(i), pos_theta(i), pos_psi(i)],...
-                                  [pos_x(i), pos_y(i), pos_hgt(i)], deg2rad(19));
+                                  [pos_x(i), pos_y(i), pos_hgt(i)], deg2rad(0));
     c_n_1(:,i) = x_temp;
     c_n_2(:,i) = y_temp;
 end
     
 plot(c_n_1(2,:),c_n_1(1,:),'color',[1 .4 0]);
 plot(c_n_2(2,:),c_n_2(1,:),'color',[1 .4 0]);
+
+plot(pos_y(1325), pos_x(1325),'*');
+
 xlim([-300 900]);
 ylim([0 1200]);
 xlabel('East [m]');
 ylabel('North [m]');
+%set(gca, 'fontsize', 14);
+saveas(gcf, 'fig_lin/pos_run', 'epsc');
+
+
+%% PLOT TRACKING PERF %%
+
+figure(3);
+hold on;
+grid on;
+plot(STATES(:,3), STATES(:,2));
+plot(pos_y, pos_x);
+legend('Optimized path','Flown path', 'Location', 'SE');
+xlim([-300 900]);
+ylim([0 1200]);
+set(gca, 'fontsize', 14);
+saveas(gcf, 'fig_lin/tracking', 'epsc');
+
+
+
+%% CALCULATE path ERROR %%
+path_started = 0;
+for i = 1:length(path_x(1:1325))
+    if((path_x(i) >= 0) && (~path_started))
+        path_started = 1
+    end
+    
+    if(path_started)
+        [x_temp, y_temp] = camera_pos([path_phi(i), path_theta(i), path_psi(i)],...
+                        [path_x(i), path_y(i), path_hgt(i)], 0.0);
+    
+        min_error = 100000;
+    
+        for j = 1:length(PATH(:,1))
+            dist = sqrt((PATH(j,1) - x_temp(1))^2 + (PATH(j,2) - x_temp(2))^2);
+            if dist < min_error
+                min_error = dist;
+            end
+        end
+
+        path_error(i) = min_error;
+    end
+end
+
+mean_path_error = mean(path_error)
+max_path_error = max(path_error)
+min_path_error = min(path_error)
+std_path_error = std(path_error)
+
+%% CALCULATE CUR ERROR %%
+path_started = 0;
+for i = 1:length(pos_x(1:1325))
+    if((pos_x(i) >= 0) && (~path_started))
+        path_started = 1
+        i
+    end
+    
+    if(path_started)
+        [x_temp, y_temp] = camera_pos([pos_phi(i), pos_theta(i), pos_psi(i)],...
+                        [pos_x(i), pos_y(i), pos_hgt(i)], 0.0);
+    
+        min_error = 100000;
+    
+        for j = 1:length(PATH(:,1))
+            dist = sqrt((PATH(j,1) - x_temp(1))^2 + (PATH(j,2) - x_temp(2))^2);
+            if dist < min_error
+                min_error = dist;
+            end
+        end
+
+        lin_error(i) = min_error;
+    end
+end
+
+mean_lin_error = mean(lin_error)
+max_lin_error = max(lin_error)
+min_lin_error = min(lin_error)
+std_lin_error = std(lin_error)
